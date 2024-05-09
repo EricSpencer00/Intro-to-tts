6c49b9eb4c5f6c9a937d767a5c1dd96527073b30
EricSpencer00 <119877507+EricSpencer00@users.noreply.github.com>
2024-05-09 00:37:12 -0500

    remove txt

--- tts.py
+++ tts.py
@@ -5,7 +5,7 @@ import os
 ls = os.path.dirname(os.path.abspath(__file__))
 
 def tts(text, lang='en'):
-    tts = gTTS(text = text, lang = lang, slow = True)
+    tts = gTTS(text = text, lang = lang, slow = False)
 
     new_file = "speech.mp3"
     path = os.path.join(ls, new_file)
